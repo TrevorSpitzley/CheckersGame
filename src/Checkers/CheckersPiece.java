6dd1c8ea863bee30ea09d9c6886d32ce4a163768
TrevSpitz <trevorspitzley@gmail.com>
2019-07-22 14:02:25 -0400

    Dipped a bit into the checkersPiece class

--- src/Checkers/CheckersPiece.java
+++ src/Checkers/CheckersPiece.java
@@ -1,4 +1,21 @@
 package Checkers;
 
 public class CheckersPiece {
+
+    private String color;
+    private boolean isKinged;
+    private int row = 0;
+    private int col = 0;
+    private int boundUp = 1;
+    private int boundDown = 1;
+    private int boundLeft = 1;
+    private int boundRight = 1;
+
+    public CheckersPiece(String color, boolean isKinged, int row, int col){
+        this.color = color;
+        this.isKinged = isKinged;
+        this.row = row;
+        this.col = col;
+    }
+
 }
